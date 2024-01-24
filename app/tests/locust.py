1263e8e6e66c9aa31d809ce61d15d4f0d900183c
angryong91 <91angryong@gmail.com>
2024-01-25 07:53:55 +0900

    :recycle: refactor locuts test

--- app/tests/locust.py
+++ app/tests/locust.py
@@ -1,4 +1,5 @@
 import traceback
+import random
 
 from locust import HttpUser, task, between
 
@@ -7,11 +8,18 @@ class LocustUser(HttpUser):
     wait_time = between(1, 2)
 
     @task
-    def check_task(self):
+    def create_task(self):
         try:
-            response = self.client.post("/api/v1/shorts/short-links",
+            self.client.post("/api/v1/shorts/short-links",
                                         json={"url": "https://airbridge.io"})
-            short_id = response.json()["shortId"]
-            self.client.get(f"/api/v1/shorts/r/{short_id}")
+
+        except:
+            print(traceback.format_exc())
+
+    @task
+    def get_task(self):
+        try:
+            short_id_list = ["108", "10C", "10f", "10j", "10Jc", "10L", "10O", "10p", "10y", "10Z"]
+            self.client.get(f"/api/v1/shorts/r/{short_id_list[random.randint(0, 9)]}")
         except:
             print(traceback.format_exc())
